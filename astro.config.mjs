d9eafade6468c29d8c4903935b43c068e1764b8b
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-03-05 18:03:58 +0100

    Vercel image service output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 
-// import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
-  // output: "server",
-  // adapter: vercel()
+  output: "server",
+  adapter: vercel({
+    imageService: true,
+    includeFiles: ["public/**/*"]
+  })
 });
