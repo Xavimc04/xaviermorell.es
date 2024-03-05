cce5d4193367d639a10661bd0343fbc7a3f8246e
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-03-05 18:07:08 +0100

    Vercel adapter items removed fot static site generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,7 @@ import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
-  output: "server",
-  adapter: vercel({
-    imageService: true,
-    includeFiles: ["public/**/*"]
-  })
 });
